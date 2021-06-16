593e4cdd43584e225c92fc5afbc17f636a42b963
Bruno <bruno_cris_santos@hotmail.com>
2021-06-16 14:00:48 -0300

    Modificação syles

--- assets/js/lista-tarefa.js
+++ assets/js/lista-tarefa.js
@@ -1,11 +1,12 @@
 const CriarTarefa = (evento) => {
 
     evento.preventDefault()
+
     const capturarInput = document.querySelector('[data-form-input]')
     const lista = document.querySelector('[data-list]')
     const valor = capturarInput.value
     if (valor.length == 0) {
-        alert('Campo vazio')
+        alert('Campo Não pode se vazio')
     } else {
         const tarefa = document.createElement('li')
         tarefa.classList.add('task')
@@ -28,16 +29,12 @@ const BotaoConcluir = () =>{
     const btnConcluir = document.createElement('button')
     btnConcluir.classList.add('check-button')
     btnConcluir.innerHTML = 'concluir'
+    btnConcluir.style.background = 'green'
+   
+
 
     btnConcluir.addEventListener('click',ConcluiTarefa)
     return btnConcluir
-}
-
-const ConcluiTarefa = (evento) =>{
-
-    const botaoConclui = evento.target
-    const tarefaCompleta = botaoConclui.parentElement
-    tarefaCompleta.classList.toggle('done')
 }
 
 const BotaoDeletar = () => {
@@ -45,13 +42,23 @@ const BotaoDeletar = () => {
     const btnDeleta = document.createElement('button')
     btnDeleta.innerHTML = 'Deletar'  
     btnDeleta.addEventListener('click',DeletarTarefa)
+    btnDeleta.style.background = 'red'
     
     return btnDeleta
 }
+const ConcluiTarefa = (evento) =>{
+
+    const botaoConclui = evento.target
+    const tarefaCompleta = botaoConclui.parentElement
+    tarefaCompleta.classList.toggle('done')
+}
+
+
 
 const DeletarTarefa = (evento) => {
     const btnDeleta = evento.target
     const tarefaCompleta = btnDeleta.parentElement
     tarefaCompleta.remove()
+    alert('successfully removed!')
 
 }
